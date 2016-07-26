6c2bd2ed64b09b937bc602edb0078b153d00798f
moslem <moslem.ezati@eskano.com>
2016-07-26 18:36:21 +0430

    add structure

--- app.home/app.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by moslem on 7/26/2016.
- */
-angular.module('app', [
-    'indexCtrl'
-]);
--- app.home/indexController.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by moslem on 7/26/2016.
- */
-angular.module('app').controller('indexCtrl', ['$scope', function($scope){
-    $scope.title = 'aaaaaaaaaaa';
-}]);
