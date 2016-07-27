dc720154eaf88acaed8dd804da04afdb0cc1eff4
moslem <moslem.ezati@eskano.com>
2016-07-27 18:50:01 +0430

    add code complexity

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,7 +317,24 @@ module.exports = function (grunt) {
         }]
       }
     },
-
+    complexity: {
+      generic: {
+        src: ['grunt.js', 'tasks/grunt-complexity.js'],
+        exclude: ['doNotTest.js'],
+        options: {
+          breakOnErrors: true,
+          jsLintXML: 'report.xml',         // create XML JSLint-like report
+          checkstyleXML: 'checkstyle.xml', // create checkstyle report
+          pmdXML: 'pmd.xml',               // create pmd report
+          errorsOnly: false,               // show only maintainability errors
+          cyclomatic: [3, 7, 12],          // or optionally a single value, like 3
+          halstead: [8, 13, 20],           // or optionally a single value, like 8
+          maintainability: 100,
+          hideComplexFunctions: false,     // only display maintainability
+          broadcast: false                 // broadcast data over event-bus
+        }
+      }
+    },
     htmlmin: {
       dist: {
         options: {
@@ -480,4 +497,5 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+  grunt.registerTask('complexity', 'complexity');
 };
